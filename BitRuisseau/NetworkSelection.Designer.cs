37a4a73c9e75acd5fbdcdb0ea88adc39f0fd13d6
Lucas Simões Pólvora <pd57mgs@eduvaud.ch>
2024-11-22 14:38:30 +0100

    Feat(Choose Network): Choose to whick broker connect
    90m-DONE-#14

---
+++ BitRuisseau/NetworkSelection.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace BitRuisseau
+{
+    partial class NetworkSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            HostTextBox = new TextBox();
+            hostLabel = new Label();
+            UsernameLabel = new Label();
+            UsernameTextBox = new TextBox();
+            label1 = new Label();
+            PasswordTextBox = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            PortValue = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)PortValue).BeginInit();
+            SuspendLayout();
+            // 
+            // HostTextBox
+            // 
+            HostTextBox.Location = new Point(211, 58);
+            HostTextBox.Name = "HostTextBox";
+            HostTextBox.Size = new Size(217, 23);
+            HostTextBox.TabIndex = 0;
+            // 
+            // hostLabel
+            // 
+            hostLabel.AutoSize = true;
+            hostLabel.Location = new Point(156, 61);
+            hostLabel.Name = "hostLabel";
+            hostLabel.Size = new Size(32, 15);
+            hostLabel.TabIndex = 1;
+            hostLabel.Text = "Host";
+            // 
+            // UsernameLabel
+            // 
+            UsernameLabel.AutoSize = true;
+            UsernameLabel.Location = new Point(128, 124);
+            UsernameLabel.Name = "UsernameLabel";
+            UsernameLabel.Size = new Size(60, 15);
+            UsernameLabel.TabIndex = 3;
+            UsernameLabel.Text = "Username";
+            // 
+            // UsernameTextBox
+            // 
+            UsernameTextBox.Location = new Point(211, 121);
+            UsernameTextBox.Name = "UsernameTextBox";
+            UsernameTextBox.Size = new Size(217, 23);
+            UsernameTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(131, 184);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Password";
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.Location = new Point(211, 181);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.PasswordChar = '*';
+            PasswordTextBox.Size = new Size(217, 23);
+            PasswordTextBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(211, 232);
+            button1.Name = "button1";
+            button1.Size = new Size(217, 47);
+            button1.TabIndex = 6;
+            button1.Text = "Connect";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += ConnectionButton;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(451, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 8;
+            label2.Text = "port";
+            // 
+            // PortValue
+            // 
+            PortValue.Location = new Point(497, 59);
+            PortValue.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            PortValue.Name = "PortValue";
+            PortValue.Size = new Size(72, 23);
+            PortValue.TabIndex = 9;
+            // 
+            // NetworkSelection
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(650, 330);
+            Controls.Add(PortValue);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(PasswordTextBox);
+            Controls.Add(UsernameLabel);
+            Controls.Add(UsernameTextBox);
+            Controls.Add(hostLabel);
+            Controls.Add(HostTextBox);
+            Name = "NetworkSelection";
+            Text = "NetworkSelection";
+            ((System.ComponentModel.ISupportInitialize)PortValue).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox HostTextBox;
+        private Label hostLabel;
+        private Label UsernameLabel;
+        private TextBox UsernameTextBox;
+        private Label label1;
+        private TextBox PasswordTextBox;
+        private Button button1;
+        private Label label2;
+        private NumericUpDown PortValue;
+    }
+}
---
+++ BitRuisseau/NetworkSelection.cs
@@ -0,0 +1,58 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitRuisseau
+{
+    public partial class NetworkSelection : Form
+    {
+        public IMqttClient _mqttClient { get; set; }
+        public NetworkSelection()
+        {
+            InitializeComponent();
+        }
+
+        async private void ConnectionButton(object sender, EventArgs e)
+        {
+            if(HostTextBox.Text == "" || UsernameTextBox.Text == "" || PasswordTextBox.Text == "")
+            {
+                MessageBox.Show("There is some blank information you need to complete",
+                    "Bad Inputs",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            MqttFactory factory = new MqttFactory();
+
+            _mqttClient = factory.CreateMqttClient();
+
+            MqttClientOptions options = new MqttClientOptionsBuilder()
+                .WithTcpServer(HostTextBox.Text, Convert.ToInt32(PortValue.Value))
+                .WithCredentials(UsernameTextBox.Text, PasswordTextBox.Text)
+                .WithClientId(Guid.NewGuid().ToString())
+                .Build();
+            try
+            {
+                var connectResult = _mqttClient.ConnectAsync(options).Result;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Failed to connect to the MQTT broker. Reason: {ex}", 
+                    "Connection failed", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);        
+            }
+
+            await _mqttClient.SubscribeAsync("test");
+            this.Close();
+        }
+    }
+}
