b776aa908dfcd0c040065a73840f722d4b5ff940
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-29 13:43:07 +0100

    feat(envelops): Create the different types of envelops
    5m-DONE-#13

---
+++ BitRuisseau/Classes/envelops/EnvoieCatalogue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib.Flac;
+
+namespace BitRuisseau.Classes.envelops
+{
+    public class EnvoieCatalogue
+    {
+        private List<Media> _content;
+
+        public List<Media> Content { get => _content; set => _content = value; }
+    }
+}
---
+++ BitRuisseau/Classes/envelops/EnvoieFichier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau.Classes.envelops
+{
+    public class EnvoieFichier
+    {
+        private string _content;
+
+        public string Content { get => _content; set => _content = value; }
+    }
+}
