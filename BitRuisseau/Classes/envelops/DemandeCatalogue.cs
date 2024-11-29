6e44db980631216138a1ed4f855e07578819e4b1
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-29 14:45:47 +0100

    fix(envelops): I forgot to save

--- BitRuisseau/Classes/envelops/DemandeCatalogue.cs
+++ BitRuisseau/Classes/envelops/DemandeCatalogue.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using BitRuisseau.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BitRuisseau.Classes.envelops
 {
-    public class DemandeCatalogue : GenericEnvelope
+    public class DemandeCatalogue : IJsonSerializableMessage
     {
         private string _content;
 
         public string Content { get => _content; set => _content = value; }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
 }
