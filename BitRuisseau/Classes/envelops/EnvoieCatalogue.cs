c577372d648075bff6ab33f51706f9ed5e4d8c52
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-11-29 15:02:35 +0100

    fix(envelops): forgot to save

--- BitRuisseau/Classes/envelops/EnvoieCatalogue.cs
+++ BitRuisseau/Classes/envelops/EnvoieCatalogue.cs
@@ -1,16 +1,23 @@
-﻿using System;
+﻿using BitRuisseau.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using TagLib.Flac;
 
 namespace BitRuisseau.Classes.envelops
 {
-    public class EnvoieCatalogue : GenericEnvelope
+    public class EnvoieCatalogue : IJsonSerializableMessage
     {
         private List<Media> _content;
 
         public List<Media> Content { get => _content; set => _content = value; }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
 }
--- BitRuisseau/Classes/envelops/EnvoieFichier.cs
+++ BitRuisseau/Classes/envelops/EnvoieFichier.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using BitRuisseau.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BitRuisseau.Classes.envelops
 {
-    public class EnvoieFichier : GenericEnvelope
+    public class EnvoieFichier : IJsonSerializableMessage
     {
         private string _content;
 
         public string Content { get => _content; set => _content = value; }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
     }
 }
